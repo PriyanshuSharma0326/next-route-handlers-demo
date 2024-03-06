24f2b18bc84c7173c768d0758973595a3ba23ab9
PriyanshuSharma0326 <ps940016@gmail.com>
2024-03-06 09:17:37 +0530

    handling get request

---
+++ src/app/comments/data.js
@@ -0,0 +1,16 @@
+const comments = [
+    {
+        id: 1,
+        message: 'This is the first comment'
+    },
+    {
+        id: 2,
+        message: 'This is the second comment'
+    },
+    {
+        id: 3,
+        message: 'This is the third comment'
+    },
+];
+
+export default comments;
