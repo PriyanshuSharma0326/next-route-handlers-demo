191ffcf0c2433ec6f61b23039542f2b26f0366a3
PriyanshuSharma0326 <ps940016@gmail.com>
2024-03-06 15:02:57 +0530

    handling post request

--- src/app/comments/api/route.js
+++ src/app/comments/api/route.js
@@ -3,3 +3,21 @@ import comments from "../data";
 export async function GET() {
     return Response.json(comments);
 }
+
+export async function POST(request) {
+    const comment = await request.json()
+
+    const newComment = {
+        id: comments.length + 1,
+        message: comment.message,
+    }
+
+    comments.push(newComment);
+
+    return new Response(JSON.stringify(newComment), {
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        status: 201
+    });
+}
