f37985f49d63645f62cc034e78071122ed783b2b
Priyanshu Sharma <ps940016@gmail.com>
2024-08-20 20:32:21 +0530

    route handlers requests: post, patch and delete

--- src/app/comments/api/[commentID]/route.js
+++ src/app/comments/api/[commentID]/route.js
@@ -1,30 +1,100 @@
-import { redirect } from "next/navigation";
+// import { redirect } from "next/navigation";
 import comments from "../../data";
 
 export async function GET(_request, { params }) {
     const filteredComment = comments.filter(item => item.id == params.commentID);
 
-    if(filteredComment.length) {
-        return Response.json(filteredComment);
+    if (filteredComment.length) {
+        return new Response(JSON.stringify(filteredComment), {
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            status: 200,
+        });
     }
     else {
-        redirect('/comments/api');
+        return new Response(JSON.stringify({ error: 'Comment not found' }), {
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            status: 404,
+        });
     }
+
+    // You can also redirect to any different api route
+    // redirect('/comments/api');
 }
 
 export async function PATCH(request, { params }) {
     const { message } = await request.json();
-
     const commentIndex = comments.findIndex(item => item.id == params.commentID);
-    comments[commentIndex].message = message;
-    
-    return Response.json(comments[commentIndex]);
+
+    if(commentIndex === -1) {
+        return new Response(JSON.stringify(
+            {
+                status: "10001",
+                message: "Comment not found!"
+            }),
+            {
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                status: 404
+            }
+        );
+    }
+    else {
+        comments[commentIndex].message = message;
+
+        return new Response(JSON.stringify(
+            {
+                status: "10000",
+                message: "Comment updated!",
+                data: comments[commentIndex]
+            }), 
+            {
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                status: 200
+            }
+        );
+    }
 }
 
 export async function DELETE(_request, { params }) {
     const commentIndex = comments.findIndex(item => item.id == params.commentID);
-    const deletedComment = comments[commentIndex];
-    comments.splice(commentIndex, 1);
 
-    return Response.json(deletedComment);
+    if(commentIndex === -1) {
+        return new Response(JSON.stringify(
+            {
+                status: "10001",
+                message: "Comment not found!"
+            }),
+            {
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                status: 404
+            }
+        );
+    }
+    else {
+        const deletedComment = comments[commentIndex];
+        comments.splice(commentIndex, 1);
+
+        return new Response(JSON.stringify(
+            {
+                status: "10000",
+                message: "Comment deleted!",
+                data: deletedComment
+            }),
+            {
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                status: 200
+            }
+        );
+    }
 }
--- src/app/comments/api/route.js
+++ src/app/comments/api/route.js
@@ -2,13 +2,56 @@ import comments from "../data";
 
 export async function GET(request) {
     const url = new URL(request.url);
-    const query = url.searchParams.get("query");
+    const query = url.searchParams.get("search");
 
-    const result = query ? comments.filter(item => item.message.includes(query)) : comments;
+    if(query) {
+        const result = comments.filter(item => item.message.includes(query));
 
-    const filteredComments = result.length ? result : comments;
-
-    return Response.json(filteredComments);
+        if(result.length > 0) {
+            return new Response(JSON.stringify(
+                {
+                    status: "10000",
+                    message: "Found",
+                    data: result,
+                }),
+                {
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    status: 200
+                }
+            );
+        }
+        else {
+            return new Response(JSON.stringify(
+                {
+                    status: "10001",
+                    message: "Comment not found",
+                }),
+                {
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    status: 404
+                }
+            );
+        }
+    }
+    else {
+        return new Response(JSON.stringify(
+            {
+                status: "10000",
+                message: "All Comments",
+                data: comments,
+            }),
+            {
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                status: 200
+            }
+        );
+    }
 }
 
 export async function POST(request) {
@@ -21,10 +64,17 @@ export async function POST(request) {
 
     comments.push(newComment);
 
-    return new Response(JSON.stringify(newComment), {
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        status: 201
-    });
+    return new Response(JSON.stringify(
+        {
+            status: "10000",
+            message: "Comment created!",
+            data: newComment
+        }),
+        {
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            status: 201
+        }
+    );
 }
